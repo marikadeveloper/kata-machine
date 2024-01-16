920421e91a4fa45aa56d96de7577fb4b413f3ac4
Marika Mella <marika.developer@gmail.com>
2024-01-16 19:03:19 +0100

    added binary search

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,24 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
+  // base case 1, needle not found
+  if (haystack.length <= 1 && haystack[0] !== needle) {
+    return false;
+  }
 
+  const middle = Math.floor(haystack.length / 2);
+
+  // base case 2, the middle needle is the right one
+  if (haystack[middle] === needle) {
+    return true;
+  }
+
+  // recursive case
+  if (haystack[middle] > needle) {
+    // if the middle number is greater than needle, start searching the left part
+    const left = haystack.slice(0, middle);
+    return bs_list(left, needle);
+  } else {
+    // if the middle number is smaller than needle, start searching the right part
+    const right = haystack.slice(middle);
+    return bs_list(right, needle);
+  }
 }
